101802aa714454a3031f754f682581fc4c5ff1ff
Joseph Rian B. Cirunay <129819800+rianbangs@users.noreply.github.com>
2025-05-01 23:01:20 -0600

    Update index.js
    change font size for testing

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 // ✅ Create MUI theme with global font
 const theme = createTheme({
   typography: {
-    fontSize: 14, 
+    fontSize: 5, 
     fontFamily: 'Roboto, sans-serif',
   },
 });

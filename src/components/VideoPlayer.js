a78822d5e7fb7f749ad1a36158567cc7e13819b2
Joseph Rian B. Cirunay <129819800+rianbangs@users.noreply.github.com>
2025-05-01 22:31:48 -0600

    Update VideoPlayer.js
    display player video height for test

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -45,7 +45,7 @@ const VideoPlayer = ({ video, onEnd }) => {
 
   return (
     <div>
-      <h3>{video.snippet.title}</h3>
+      <h3>{video.snippet.title}{playerHeight}</h3>
       <YouTube videoId={videoId} opts={opts} onEnd={handleEnd} />
     </div>
   );

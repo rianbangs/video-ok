d6351c841cce88ea55ba3935b7d26a1001cac3eb
Joseph Rian B. Cirunay <129819800+rianbangs@users.noreply.github.com>
2025-05-01 22:43:43 -0600

    Update VideoPlayer.js
    video height testing

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -13,9 +13,9 @@ const VideoPlayer = ({ video, onEnd }) => {
       console.log(`Screen dimensions: ${screenWidth}x${screenHeight}`);
   
       if (screenWidth >= 1366 && screenWidth <= 1920 && screenHeight >= 900) {
-        setPlayerHeight('900');
+        setPlayerHeight('850');
       } else {
-        setPlayerHeight('650');
+        setPlayerHeight('630');
         
       }
     };
